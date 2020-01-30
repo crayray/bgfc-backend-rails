880c262ef9e232436914e0737ab8a75b1f4df51f
rachfuerst <rach.fuerst@gmail.com>
2020-01-30 10:34:51 -0600

    Getting the serializer to halfway work: the profile data is inside /users/id, however it's not nested

---
+++ app/serializers/profile_serializer.rb
@@ -0,0 +1,5 @@
+class ProfileSerializer < ActiveModel::Serializer
+    attributes :id, :name, :interest1, :instagram, :facebook, :twitter, :linkedin, :user_id
+    belongs_to :user
+    # has_many :user_teams, serializer: UserTeamSerializer
+  end
