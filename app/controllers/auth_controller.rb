ab4c14b86ce475f240feefda79de69fe8aad4424
rachfuerst <rach.fuerst@gmail.com>
2020-02-01 13:20:43 -0600

    Login working from frontend with backend

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,7 +2,7 @@ class AuthController < ApplicationController
     skip_before_action :authorized, only: [:create]
  
     def create
-      @user = User.find_by(username: user_login_params[:username])
+      @user = User.find_by(email: user_login_params[:email])
       #User#authenticate comes from BCrypt
       if @user && @user.authenticate(user_login_params[:password])
         # encode token comes from ApplicationController
@@ -17,6 +17,6 @@ class AuthController < ApplicationController
    
     def user_login_params
       # params { user: {username: 'Chandler Bing', password: 'hi' } }
-      params.require(:user).permit(:username, :password)
+      params.require(:user).permit(:email, :password)
     end
 end
