21d53e22a1a749934b87e250087bb74bfff0ae4f
rachfuerst <rach.fuerst@gmail.com>
2020-02-02 07:34:47 -0600

    Added a small space in order to push to remote after making this branch HEAD

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-#Users
+#Users 
 rachel= User.create(name:"rachel", email:"rach@gmail.com", password_digest: "1234")
 
 
