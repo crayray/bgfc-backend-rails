8beea83ca905504549cd3594723517ab52bcc3e7
rachfuerst <rach.fuerst@gmail.com>
2020-02-05 09:46:07 -0600

    Adding back the profile serializer, since it was causing a bug with login

--- app/serializers/profile_serializer.rb
+++ app/serializers/profile_serializer.rb
@@ -1,7 +1,7 @@
-# class ProfileSerializer < ActiveModel::Serializer
-#     attributes :id, :name, :interest1, :instagram, :facebook, :twitter, :linkedin, :user_id
-#     belongs_to :user
+class ProfileSerializer < ActiveModel::Serializer
+    attributes :id, :name, :interest1, :instagram, :facebook, :twitter, :linkedin, :user_id
+    belongs_to :user
 
     
-#     # has_many :user_teams, serializer: UserTeamSerializer
-#   end
+    # has_many :user_teams, serializer: UserTeamSerializer
+  end
