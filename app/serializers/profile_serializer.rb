de1f45b90f1778b59790f9714940afc6ba8757ca
rachfuerst <rach.fuerst@gmail.com>
2020-02-05 10:29:35 -0600

    Trying out url serializer from https://medium.com/@elenichappen/using-active-storage-with-active-model-serializers-36c91f5a0686

--- app/serializers/profile_serializer.rb
+++ app/serializers/profile_serializer.rb
@@ -1,7 +1,11 @@
 class ProfileSerializer < ActiveModel::Serializer
-    attributes :id, :name, :interest1, :instagram, :facebook, :twitter, :linkedin, :user_id
+  include Rails.application.routes.url_helpers
+    attributes :id, :name, :interest1, :instagram, :facebook, :twitter, :linkedin, :user_id, :avatar_url
     belongs_to :user
-
+    def avatar_url
+      variant = object.avatar.variant(resize: "100x100")
+      return rails_representation_url(variant, only_path: true)
+    end
     
     # has_many :user_teams, serializer: UserTeamSerializer
   end
